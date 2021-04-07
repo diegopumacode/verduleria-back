e9ec90142cb7bd8841876e05aa1135c61ac75f08
diegopumacode <diegopumacode@gmail.com>
2021-04-07 13:30:24 -0500

    adding model: Product

--- src/models/Product/Product.ts
+++ src/models/Product/Product.ts
@@ -1,24 +1,34 @@
-import { Product } from "./../Product/Product";
+import { Category } from "./../Category/Category";
 import {
   BaseEntity,
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 
 @Entity()
-export class Category extends BaseEntity {
+export class Product extends BaseEntity {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column({ type: "varchar", length: 100 })
   name: string;
 
-  @OneToMany(() => Product, (product) => product.category)
-  products: Product[];
+  @Column({ type: "varchar", length: 100 })
+  description: string;
+
+  @Column({ type: "int" })
+  stock: number;
+
+  @Column({ type: "float" })
+  price: number;
+
+  @ManyToOne(() => Category, (category) => category.products)
+  category: Category;
 
   @CreateDateColumn()
   create_at: string;
